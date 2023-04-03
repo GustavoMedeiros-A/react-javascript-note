ef0516c740ac52b1a67cf12f9e3fdba04e5a1af3
Gustavo <gustavomedeiros15@outlook.com.br>
2023-04-03 17:14:22 -0300

    Configurando o bulma

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import "./App.scss";
+
+const App = () => (
+
+  <Fragment>
+    <div className="section">
+      <p>JavaScript Notes!</p>
+    </div>
+  </Fragment>
+
+);
+
+export default App;
